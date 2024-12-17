cf75a525c0a292596960439019117ff32dfb925c
jan-szczuka5 <janogamesyt5@gmail.com>
2024-12-17 17:11:31 +0100

    delete comments

--- task_B/test.py
+++ task_B/test.py
@@ -2,16 +2,16 @@ import pytest
 from b import isBalanced
 
 @pytest.mark.parametrize("tree, expected", [
-    (None, True),  # Empty tree
-    ([None, None], True),  # Single node tree
-    ([[None, None], [None, None]], True),  # Balanced tree with two levels
-    ([[[None, None], None], [None, None]], True),  # Balanced tree with three levels
-    ([[[None, None], [None, None]], [None, None]], True),  # Balanced tree with three levels
-    ([[[None, None], None], None], False),  # Unbalanced tree with three levels
-    ([[None, [None, None]], [None, None]], True),  # Balanced tree with three levels
-    ([[None, [None, None]], None], False),  # Unbalanced tree with three levels
-    ([[[None, None], [None, None]], [[None, None], [None, None]]], True),  # Balanced tree with four levels
-    ([[[None, None], [None, None]], [None, None]], True)  # Balanced tree with three levels
+    (None, True),
+    ([None, None], True),
+    ([[None, None], [None, None]], True),
+    ([[[None, None], None], [None, None]], True),
+    ([[[None, None], [None, None]], [None, None]], True),
+    ([[[None, None], None], None], False),
+    ([[None, [None, None]], [None, None]], True),
+    ([[None, [None, None]], None], False),
+    ([[[None, None], [None, None]], [[None, None], [None, None]]], True),
+    ([[[None, None], [None, None]], [None, None]], True)
 ])
 def test_isBalanced(tree, expected):
     assert isBalanced(tree) == expected
